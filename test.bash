082191344cc97d2345e88bf531e1cf40a105c1fc
Shuma Kasai <kasai.0124.sss@gmail.com>
2022-11-29 02:32:56 +0900

    add multiply

--- test.bash
+++ test.bash
@@ -12,15 +12,26 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+out=$(seq 5 | ./multiply)
+[ "${out}" = 120 ] || ng ${LINENO}
+
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]	  || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
+out=$(echo あ | ./multiply)
+[ "$?" = 1 ]	  || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 
 out=$(echo | ./plus)
 [ "$?" = 1 ]	  || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
+out=$(echo | ./multiply)
+[ "$?" = 1 ]	  || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
